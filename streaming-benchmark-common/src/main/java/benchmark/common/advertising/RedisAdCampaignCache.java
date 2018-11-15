95b099654666eb036e519b29aade3c8d9ffb4d2d
Elkoumy <gamal.elkomy@gmail.com>
2018-11-15 18:20:32 +0200

    adding redis pool inside code to avoid timeout.

--- streaming-benchmark-common/src/main/java/benchmark/common/advertising/RedisAdCampaignCache.java
+++ streaming-benchmark-common/src/main/java/benchmark/common/advertising/RedisAdCampaignCache.java
@@ -5,21 +5,35 @@
 package benchmark.common.advertising;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
+import javax.security.auth.login.Configuration;
 import java.util.HashMap;
 
 public class RedisAdCampaignCache {
     private Jedis jedis;
+    private JedisPool jedisPool;
     private HashMap<String, String> ad_to_campaign;
+    private final int HOUR_IN_SECONDS = 3600 * 10;  // 10 HOURS
+
 
     public RedisAdCampaignCache(String redisServerHostname) {
-        jedis = new Jedis(redisServerHostname);
+//        jedis = new Jedis(redisServerHostname);
+//        this.config = config;
+        jedisPool = new JedisPool(buildJedisPoolConfiguration(), redisServerHostname, 6379, HOUR_IN_SECONDS, null);
+        jedis = jedisPool.getResource();
     }
 
     public void prepare() {
         ad_to_campaign = new HashMap<String, String>();
     }
 
+    private JedisPoolConfig buildJedisPoolConfiguration() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxTotal(1000);
+        return jedisPoolConfig;
+    }
     public String execute(String ad_id) {
         String campaign_id = ad_to_campaign.get(ad_id);
         if(campaign_id == null) {
