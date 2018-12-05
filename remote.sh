119a027078729c525e4be7097620578a1baf989e
Elkoumy <gamal.elkomy@gmail.com>
2018-12-05 14:07:38 +0200

    tps change

--- remote.sh
+++ remote.sh
@@ -587,10 +587,10 @@ function benchmarkLoop (){
 #            scp -r ${SSH_USER}@stream-node-01:${PROJECT_DIR}/result/flink/* ${PROJECT_DIR}/resultLogs/${ALGORITHM}/result/$(date +%Y-%m-%d_%H%M%S)_TPS_${TPS}
             TPS=$[$TPS + $TPS_RANGE]
 
-#            runCommandKafkaServers "reboot"
+            runCommandKafkaServers "reboot"
 #            runCommandLoadServers "reboot"
 #            runCommandZKServers "reboot"
-#            sleep ${WAIT_AFTER_REBOOT_SERVER}
+            sleep 120
 
         done
 
