1be7edfeda3908cb6c88e1c125f6d402d438d531
Elkoumy <gamal.elkomy@gmail.com>
2018-12-12 18:12:19 +0200

    adding loader in the file.

--- stream-bench.sh
+++ stream-bench.sh
@@ -310,7 +310,7 @@ run() {
     cd data
 #    start_if_needed leiningen.core.main "Load Generation" 1 $LEIN run -r -t $TPS --configPath ../$CONF_FILE
         #!/bin/bash
-        java -cp ./data/Stream-Data-Generator.jar  ee.ut.cs.dsg.datagenrator.Main 100 1000 RR kafka &
+        java -cp "/root/stream-benchmarking/data/Stream-Data-Generator.jar"  ee.ut.cs.dsg.datagenrator.Main 100 1000 RR kafka &
         loaderPid=$!
 
 
