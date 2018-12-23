a96db577fe66e486a22a2b2d0fb470c05a453ac4
Mohammed Ali <43534912+Mohmmed-Ali@users.noreply.github.com>
2018-12-23 17:28:18 +0200

    fix writing to redis  19

--- stream-bench.sh
+++ stream-bench.sh
@@ -232,12 +232,16 @@ run() {
   then
     start_if_needed redis-server Redis 1 "$REDIS_DIR/src/redis-server" --maxclients 100000000 --protected-mode no
     cd data
-    $LEIN run -n --configPath ../$CONF_FILE
+    #$LEIN run -n --configPath ../$CONF_FILE
+    #!/bin/bash
+    java -cp "/root/stream-benchmarking/data/ReadFromRedis.jar"  ReadFromRedis &
     cd ..
   elif [ "LOAD_FROM_REDIS" = "$OPERATION" ];
   then
     cd data
-    $LEIN run -g --configPath ../$CONF_FILE || true
+    #$LEIN run -g --configPath ../$CONF_FILE || true
+     #!/bin/bash
+    java -cp "/root/stream-benchmarking/data/ReadFromRedis.jar"  ReadFromRedis &
     cd ..
   elif [ "STOP_REDIS" = "$OPERATION" ];
   then
