14485832a314664fb702e2c4bd8b0058e3897bca
Mohammed Ali <43534912+Mohmmed-Ali@users.noreply.github.com>
2018-12-27 16:49:29 +0200

    Tracing back pressure reason 4

--- flink-benchmarks/src/main/java/flink/benchmark/StreamSqlBenchQueriesFlink3.java
+++ flink-benchmarks/src/main/java/flink/benchmark/StreamSqlBenchQueriesFlink3.java
@@ -168,7 +168,7 @@ public class StreamSqlBenchQueriesFlink3 {
          * ************************************************************/
         //purchaseWithTimestampsAndWatermarks.flatMap(new WriteToRedis());
         Table result = tEnv.sqlQuery("SELECT  userID, gemPackID, rowtime from purchasesTable");
-        //DataStream<Tuple2<Boolean, Row>> queryResultAsDataStream = tEnv.toRetractStream(result, Row.class);
+        DataStream<Tuple2<Boolean, Row>> queryResultAsDataStream = tEnv.toRetractStream(result, Row.class);
         //queryResultAsDataStream.flatMap(new WriteToRedisAfterQuery());
 
 
