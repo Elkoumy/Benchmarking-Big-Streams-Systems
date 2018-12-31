fa20de1b6b4e892ab3a1b74aea5651618094f0a2
Mohammed Ali <43534912+Mohmmed-Ali@users.noreply.github.com>
2018-12-31 15:13:25 +0200

    Running jobs 3 Group by

--- flink-benchmarks/src/main/java/flink/benchmark/StreamSqlBenchQueriesFlink3.java
+++ flink-benchmarks/src/main/java/flink/benchmark/StreamSqlBenchQueriesFlink3.java
@@ -193,14 +193,15 @@ public class StreamSqlBenchQueriesFlink3 {
         queryResultAsDataStream.flatMap(new WriteToRedisAfterQuery());*/
 
         /**************************************************************
-         * 3- Group by & having // Getting revenue from gempack when it exceeds specified amount
+         * 3- Group  // Getting revenue from gempack when it exceeds specified amount
          * TODO> I think in this kind of queries we should not calculate throughput. because we will not be able to count the filtered out tuples
          * ************************************************************/
 
         // register function
         purchaseWithTimestampsAndWatermarks.flatMap(new WriteToRedisBeforeQuery());
         tEnv.registerFunction("getKeyAndValue", new KeyValueGetter());
-        Table result = tEnv.sqlQuery("SELECT  gemPackID,sum(price)as revenue,getKeyAndValue(userID, rowtime),count(*)   from purchasesTable GROUP BY TUMBLE(rowtime, INTERVAL '10' SECOND),gemPackID HAVING sum(price)>400 ");
+//        Table result = tEnv.sqlQuery("SELECT  gemPackID,sum(price)as revenue,getKeyAndValue(userID, rowtime),count(*)   from purchasesTable GROUP BY TUMBLE(rowtime, INTERVAL '2' SECOND),gemPackID HAVING sum(price)>20 ");
+        Table result = tEnv.sqlQuery("SELECT  gemPackID,sum(price)as revenue,getKeyAndValue(userID, rowtime),count(*)   from purchasesTable GROUP BY TUMBLE(rowtime, INTERVAL '2' SECOND),gemPackID");
         //for the metrics calculation after
         DataStream<Tuple2<Boolean, Row>> queryResultAsDataStream = tEnv.toRetractStream(result, Row.class);
         queryResultAsDataStream.flatMap(new WriteToRedisAfterQuery());
