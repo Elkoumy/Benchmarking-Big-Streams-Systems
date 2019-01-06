6f6db18cd8f483d73469168296980e122b4c3293
Mohammed Ali <43534912+Mohmmed-Ali@users.noreply.github.com>
2019-01-06 12:20:42 +0200

    Run the first job. Projection fix 3

--- flink-benchmarks/src/main/java/flink/benchmark/RedisReadAndWriteAfter.java
+++ flink-benchmarks/src/main/java/flink/benchmark/RedisReadAndWriteAfter.java
@@ -135,11 +135,12 @@ public class RedisReadAndWriteAfter {
         }
     }
     private void flushWindows() {
+        Pipeline p = flush_jedis.pipelined();
         synchronized (elemensTowrite) {
 //            if(elemensTowrite.size()>0){writeWindow_Throughput(System.currentTimeMillis()+"",elemensTowrite.size()+"");}
-            Pipeline p = flush_jedis.pipelined();
+
             for (String s : elemensTowrite.keySet()) {
-                p.hset(s, "time_seen",elemensTowrite.get(s));
+                p.hset(s, "time_updated",elemensTowrite.get(s));
                 //writeWindow(s, elemensTowrite.get(s));
             }
             p.syncAndReturnAll();
