ecaeb73990fba85d46b59c71411d45c9d480bd61
Mohammed Ali <43534912+Mohmmed-Ali@users.noreply.github.com>
2019-01-11 16:37:38 +0200

    Run the ninth job, Session window.

--- flink-benchmarks/src/main/java/flink/benchmark/StreamSqlBenchQueriesFlink3.java
+++ flink-benchmarks/src/main/java/flink/benchmark/StreamSqlBenchQueriesFlink3.java
@@ -345,7 +345,7 @@ public class StreamSqlBenchQueriesFlink3 {
         /**************************************************************
          * 5- Sliding Window //Getting revenue obtained from each gem pack over fixed overlapped period of time
          * ************************************************************/
-        // register functions
+/*        // register functions
         tEnv.registerFunction("getDifferences", new DifferencesGetter());
         tEnv.registerFunction("getTheSpecialValue", new SpecialValueGetter());
         Table result = tEnv.sqlQuery("SELECT  gemPackID,sum(price)as revenue,getDifferences(ltcID),getTheSpecialValue(userID, rowtime),count(*)   from purchasesTable GROUP BY HOP(rowtime, INTERVAL '1' SECOND, INTERVAL '2' SECOND),gemPackID");
@@ -363,19 +363,30 @@ public class StreamSqlBenchQueriesFlink3 {
 
             }
         }).name("check the the last record");
-        queryResultAsDataStream.print();
+        queryResultAsDataStream.print();*/
 
         /**************************************************************
          * 6- Session window //Getting Revenue obtained from each gem pack after each specific period of inactivity
          * ************************************************************/
-/*        purchaseWithTimestampsAndWatermarks.flatMap(new WriteToRedisBeforeQuery());
-        // register function
-        tEnv.registerFunction("getKeyAndValue", new KeyValueGetter());
-        Table result = tEnv.sqlQuery("SELECT  gemPackID,sum(price)as revenue,getKeyAndValue(userID, rowtime),count(*)   from purchasesTable GROUP BY SESSION(rowtime, INTERVAL '2' SECOND),gemPackID");
+        // register functions
+        tEnv.registerFunction("getDifferences", new DifferencesGetter());
+        tEnv.registerFunction("getTheSpecialValue", new SpecialValueGetter());
+        Table result = tEnv.sqlQuery("SELECT  gemPackID,sum(price)as revenue,getDifferences(ltcID),getTheSpecialValue(userID, rowtime),count(*)   from purchasesTable GROUP BY SESSION(rowtime, INTERVAL '2' SECOND),gemPackID");
         //for the metrics calculation after
         DataStream<Tuple2<Boolean, Row>> queryResultAsDataStream = tEnv.toRetractStream(result, Row.class);
-        queryResultAsDataStream.flatMap(new WriteToRedisAfterQuery());*/
+        queryResultAsDataStream.map(new MapFunction<Tuple2<Boolean, Row>, Object>() {
+            @Override
+            public Object map(Tuple2<Boolean, Row> input) throws Exception {
+                if (input.f1.getField(3).toString().equals("-1000000")){
+                    System.exit(0);
 
+
+                }
+                return null;
+
+            }
+        }).name("check the the last record");
+        queryResultAsDataStream.print();
 
         //================================JOINS======================
         /**************************************************************
